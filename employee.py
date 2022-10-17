a92ec1607bd61a7c20499d5dd0d8b25ceaea6256
Shabrina Hoque <hoqueshabrina@gmail.com>
2022-10-17 17:45:16 +0100

    remove print statements

--- employee.py
+++ employee.py
@@ -62,26 +62,13 @@ class Employee:
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
 billie = Employee('Billie',4000,[],[])
-print(billie.get_pay())
-print(str(billie))
-
 # Charlie works on a contract of 100 hours at 25/hour.  Their total pay is 2500.
 charlie = Employee('Charlie',"",[100,25],[])
-print(charlie.get_pay())
-print(str(charlie))
 # Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract.  Their total pay is 3800.
 renee = Employee('Renee',3000,[],[4,200])
-print(renee.get_pay())
-print(str(renee))
 # Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract.  Their total pay is 4410.
 jan = Employee('Jan',"",[150,25],[3,220])
-print(jan.get_pay())
-print(str(jan))
 # Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500.  Their total pay is 3500.
 robbie = Employee('Robbie',2000,[],[1500])
-print(robbie.get_pay())
-print(str(robbie))
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel',"",[120,30],[600])
-print(ariel.get_pay())
-print(str(ariel))
